56c09d956350d296db0b6147db1eb57f69fcaf7f
Dan Priola <dpriola@good-sam.com>
2018-12-06 11:20:25 -0600

    Test
    Test

--- LockedDownKioskMode/Program.cs
+++ LockedDownKioskMode/Program.cs
@@ -18,7 +18,7 @@ namespace LockedDownKioskMode
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            //Comments
+            //Commentss
         }
     }
 }
