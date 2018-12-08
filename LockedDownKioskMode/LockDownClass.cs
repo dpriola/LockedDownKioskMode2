a77d64e306ee70c90d41d39eab6aec76bf637403
Dan Priola <dpriola@good-sam.com>
2018-12-07 19:06:48 -0600

    LockdownClass Update
    updated the Class so that the Windows key is disabled

--- LockedDownKioskMode/LockDownClass.cs
+++ LockedDownKioskMode/LockDownClass.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -25,9 +26,35 @@ namespace LockedDownKioskMode
             EsSystemRequired = 0x00000001
         }
 
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int
+            wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowsHookExA", CharSet = CharSet.Ansi)]
+        private static extern int SetWindowsHookEx(
+           int idHook,
+           LowLevelKeyboardProcDelegate lpfn,
+           int hMod,
+           int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern int UnhookWindowsHookEx(int hHook);
+
+
+        [DllImport("user32.dll", EntryPoint = "CallNextHookEx", CharSet = CharSet.Ansi)]
+        private static extern int CallNextHookEx(
+            int hHook, int nCode,
+            int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+
+        const int WH_KEYBOARD_LL = 13;
+        private int intLLKey;
+        private KBDLLHOOKSTRUCT lParam;
 
         public void main()
         {
+            KeyboardHook(this);
+            
+            /*
             RegistryKey key1 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
             key1 = key1.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Winlogon", true);
             if (key1 != null)
@@ -67,7 +94,9 @@ namespace LockedDownKioskMode
 
             key5.SetValue("NoLogoff", 1);
             key5.Close();
+            */
 
+            /*
             string root = @"C:\Users\Setup\AppData\Local\Microsoft\Windows\WinX";
             // If directory does not exist, don't even try   
             if (Directory.Exists(root))
@@ -78,6 +107,63 @@ namespace LockedDownKioskMode
                 }
                 Directory.Delete(root);
             }
+            */
+        }
+
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            int scanCode;
+            public int flags;
+            int time;
+            int dwExtraInfo;
+        }
+
+        private int LowLevelKeyboardProc(
+            int nCode, int wParam,
+            ref KBDLLHOOKSTRUCT lParam)
+        {
+            bool blnEat = false;
+            switch (wParam)
+            {
+                case 256:
+                case 257:
+                case 260:
+                case 261:
+                    //Alt+Tab, Alt+Esc, Ctrl+Esc, Windows Key
+                    if (((lParam.vkCode == 9) && (lParam.flags == 32)) ||
+                    ((lParam.vkCode == 27) && (lParam.flags == 32)) || ((lParam.vkCode ==
+                    27) && (lParam.flags == 0)) || ((lParam.vkCode == 91) && (lParam.flags
+                    == 1)) || ((lParam.vkCode == 92) && (lParam.flags == 1)) || ((true) &&
+                    (lParam.flags == 32)))
+                    {
+                        blnEat = true;
+                    }
+                    break;
+            }
+
+            if (blnEat)
+                return 1;
+            else return CallNextHookEx(0, nCode, wParam, ref lParam);
+
+        }
+
+        private void KeyboardHook(object sender)
+        {
+            intLLKey =
+                SetWindowsHookEx(
+
+                WH_KEYBOARD_LL,
+
+                new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc),
+
+                System.Runtime.InteropServices.Marshal.GetHINSTANCE(
+                  System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]).ToInt32(), 0);
+        }
+
+        private void ReleaseKeyboardHook()
+        {
+            intLLKey = UnhookWindowsHookEx(intLLKey);
         }
 
         public static void PreventSleep()
